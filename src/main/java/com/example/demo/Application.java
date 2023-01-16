a2fc87b48acf2440cd9259d1bb11c7029f0df861
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2023-01-16 15:00:53 +0100

    Add example code to run at strartup

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+
+    // Some code to run after our appl starts.. just to check the whole process
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student ali= new Student("Ali", "Mahiuddin", "ali_m@prac.com", 31);
+            studentRepository.save(ali);            
+        };
+    }
 }
