523086d616d181636ef5d3a2f8ac2f7b1908aeb9
Muneeb H. Jan <muneeb_hafeez.jan@mailbox.tu-dresden.de>
2023-01-16 14:14:23 +0100

    use sequence generator for ID value

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,12 +1,26 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import static javax.persistence.GenerationType.SEQUENCE;
 
-@Entity
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity(name = "Student") // We should explicitly define the name of entity as good practice
 public class Student {
 
-    @Id
+    @Id // Shows that id is primary key.. using @Id annotation. These are the fields in our relation Student in DB which will be created at startup
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    ) // Sequence generator is used to generate new values. That's why we want it. allocation Size is just increment step, i.e +1
+    @GeneratedValue(
+        strategy = SEQUENCE,
+        generator = "student_sequence"
+    ) // value for id comes from a sequence generated using sequence generator defined above. The type in DB will be big serial.
     private Long id;
     private String firstName;
     private String lasName;
